5fbad4292f10248588bf638474691afdb85f1770
DevolperPHP <devolperphp@gmail.com>
2025-05-26 16:25:16 +0300

    Create Remove String Spaces.js

---
+++ Easy/Remove String Spaces.js
@@ -0,0 +1,9 @@
+/*
+Write a function that removes the spaces from the string, then return the resultant string.
+
+Examples (Input -> Output):
+*/
+
+function noSpace(x){
+  return x.replaceAll(" ", "")
+}
